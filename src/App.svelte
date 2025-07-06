401c608c0c5210c0528cb88464e00359ddbb86e7
hashiryo <hashimotoryoma@gmail.com>
2025-07-06 19:56:33 +0900

    feat: refactor App component to use GameBoard and remove direct Piece and Board imports

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
-  import Piece from './feature/game-board/Piece.svelte';
-  import Board from './feature/game-board/Board.svelte';
   import GameBoard from './feature/game-board/GameBoard.svelte';
 </script>
 
 <main>
   <!-- <GameBoard/> -->
-  <Board />
-  <Piece />
-  <Piece reverse={true}/>
+  <GameBoard />
 </main>
 
 <style>
