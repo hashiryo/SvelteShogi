802cadbce04576c1e1e57ea2df097b406faf40ef
hashiryo <hashimotoryoma@gmail.com>
2025-07-11 18:11:21 +0900

    feat: 矢印の透明度を追加し、表示スタイルを改善

--- src/ui/game-canvas/information/Arrow.svelte
+++ src/ui/game-canvas/information/Arrow.svelte
@@ -6,6 +6,7 @@
     endY = 0,
     color = { r: 0, g: 0, b: 255 }, // RGB color
     width = 30,
+    opacity = 1.0,
   } = $props();
 
   const length = Math.sqrt((endX - startX) ** 2 + (endY - startY) ** 2);
@@ -18,11 +19,12 @@
   class="arrow"
   style="width: {length}px;
          height: {width}px;
-         background: {`rgba(${color.r}, ${color.g}, ${color.b}, 0.5)`};
+         background: {`rgba(${color.r}, ${color.g}, ${color.b}, ${0.5 * opacity})`};
          left: {startX}px;
          top: {startY - width / 2}px; /* 矢印の中心を合わせるために半分引く */
          clip-path: polygon(0 0, {marker}% 0, 100% 50%, {marker}% 100%, 0 100%);
          transform: rotate({angle}deg);
+         opacity: {opacity};
          "
 ></div>
 
