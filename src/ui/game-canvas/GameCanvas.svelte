c50dedb1048f4aa36734750870e861207a1e4033
hashiryo <hashimotoryoma@gmail.com>
2025-07-11 16:03:41 +0900

    feat: GameCanvasコンポーネントにArrowコンポーネントを追加し、矢印の描画機能を実装

--- src/ui/game-canvas/GameCanvas.svelte
+++ src/ui/game-canvas/GameCanvas.svelte
@@ -1,9 +1,29 @@
 <script lang="ts">
   import GameBoard from './game-board/GameBoard.svelte';
+  import Arrow from './information/Arrow.svelte';
   import type { PieceType, PieceOnBoard } from '../../types/shogi.d.ts';
 
   // 盤上の各マスのDOM情報を格納する配列 (Boardコンポーネントから受け取る)
   let squareElements: HTMLDivElement[] = $state([]);
+
+  // キャンバス全体のコンテナ要素とその座標
+  let canvasElement: HTMLDivElement | undefined = $state();
+
+  let relativeSquarePositions: {x: number, y: number}[] = $derived((() => {
+    // squarePositionsが更新されたときに、相対座標を計算
+      const canvasRect = canvasElement?.getBoundingClientRect();
+      if (canvasRect) {
+        // ボードの座標が取得できた場合、相対座標を計算
+        return squareElements.map(el => {
+          const pos = el.getBoundingClientRect();
+          return {
+            x: pos.left - canvasRect.left,
+            y: pos.top - canvasRect.top
+          };
+        });
+      }
+      return squareElements.map(() => ({ x: 0, y: 0 }));
+  })());
 
   // 仮の盤上の駒データ
   let piecesOnBoard: PieceOnBoard[] = [
@@ -39,16 +59,33 @@
 
 // let reverse = false; // 盤の向きを反転するかどうか
   let reverse = true; // 盤の向きを反転するかどうか
+
+
+  // 矢印
+  let arrows = [{
+    start: { row: 6, col: 0 },
+    end: { row: 5, col: 0 },
+    length: 50,
+    winningRate: 0.5,
+    appearanceRate: 0.5,
+  }]
 </script>
 
-<main>
+<div class="canvas" bind:this={canvasElement}>
   <GameBoard {piecesOnBoard} 
              {capturedPiecesMe} 
              {capturedPiecesOpponent} 
              {reverse} 
              bind:squareElements={squareElements} />
-</main>
+  {#each arrows as arrow}
+    <Arrow winningRate={arrow.winningRate} appearanceRate={arrow.appearanceRate} strokeLength={arrow.length} maxWidth={50} />
+  {/each}
+</div>
 
 <style>
-
+.canvas {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
 </style>
---
+++ src/ui/game-canvas/information/Arrow.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  let {
+    winningRate = 0.5,
+    appearanceRate = 0.5,
+    strokeLength = 100,
+    maxWidth = 50,
+  } = $props();
+
+  function getStrokeColor(v: number) {
+    let r = 0, g = 0, b = 0;
+    if (v < 0.5) {
+      const t = v / 0.5;
+      r = 0;
+      g = Math.round(255 * t);
+      b = Math.round(255 * (1 - t));
+    } else {
+      const t = (v - 0.5) / 0.5;
+      r = Math.round(255 * t);
+      g = Math.round(255 * (1 - t));
+      b = 0;
+    }
+    return `rgba(${r}, ${g}, ${b}, 0.8)`;
+  }
+</script>
+
+<div class="arrow"
+     style="background:{getStrokeColor(winningRate)};
+            width: {strokeLength}px;
+            height: {appearanceRate * maxWidth}px;"
+></div>
+
+<style>
+.arrow {
+  display: inline-block;
+  clip-path: polygon(0 0, 90% 0, 100% 50%, 90% 100%, 0 100%);
+}
+</style>
